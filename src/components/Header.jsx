61c7abe57d2922bfdf888d3dcb9d5a32d974d521
Tassio Neves <tassions@hotmail.com>
2025-02-13 15:36:18 -0300

    refactor: change hiperlink to Link react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import spotifyLogo from "../assets/logo/spotify-logo.png";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <a to="/">
+      <Link to="/">
         <img width="32" src={spotifyLogo} alt="Logo Spotify" />
-      </a>
-      <a to="/">
+      </Link>
+      <Link to="/">
         <h1>Spotify</h1>
-      </a>
+      </Link>
     </div>
   );
 };
