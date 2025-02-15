74c2f35ca1adee86bf38d8980afce7c65f0e9283
Tassio Neves <tassions@hotmail.com>
2025-02-15 14:20:38 -0300

    feat: add conection with api and

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ItemList from "./ItemList";
-import { artistArray } from "../assets/database/artists";
+import { artistsArray } from "../assets/database/artists";
 import { songsArray } from "../assets/database/songs";
 
 const Main = ({ type }) => {
@@ -11,7 +11,7 @@ const Main = ({ type }) => {
         <ItemList
           title="Artistas"
           items={5}
-          itemsArray={artistArray}
+          itemsArray={artistsArray}
           path="/artists"
           idPath="/artist"
         />
--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -3,15 +3,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 import { Link, useParams } from "react-router-dom";
 import SongList from "../components/SongList";
-import { artistArray } from "../assets/database/artists";
+import { artistsArray } from "../assets/database/artists";
 import { songsArray } from "../assets/database/songs";
 
 const Artist = () => {
   const { id } = useParams();
   // console.log(useParams());
 
-  const { name, banner } = artistArray.filter(
-    (currentArtistObj) => currentArtistObj.id === Number(id)
+  const { name, banner } = artistsArray.filter(
+    (currentArtistObj) => currentArtistObj._id === id
   )[0];
 
   const songsArrayFromArtist = songsArray.filter(
@@ -21,7 +21,7 @@ const Artist = () => {
   const randomIndex = Math.floor(
     Math.random() * (songsArrayFromArtist.length - 1)
   );
-  const randomIdFromArtist = songsArrayFromArtist[randomIndex].id;
+  const randomIdFromArtist = songsArrayFromArtist[randomIndex]._id;
 
   // console.log(randomIdFromArtist);
   // console.log(Math.floor(Math.random() * (songsArrayFromArtist.length - 1)));
